bdf6fc15d2dd7ce4aa9b85d400a1b551bb8169f0
Joonas Blomberg <joonas.blomberg@gmail.com>
2018-10-07 11:54:00 +0300

    Camel app

---
+++ src/main/java/org/feedtube/camel/CamelRouter.java
@@ -0,0 +1,12 @@
+package org.feedtube.camel;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CamelRouter extends RouteBuilder {
+    @Override
+    public void configure() throws Exception {
+
+    }
+}
